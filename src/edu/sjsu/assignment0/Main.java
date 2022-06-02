882efcbebd2452d07ff3eeaec812af67ffc37b00
Yan Chen <yan.chen01@sjsu.edu>
2022-06-02 12:36:08 -0700

    Just demo

--- src/edu/sjsu/assignment0/Main.java
+++ src/edu/sjsu/assignment0/Main.java
@@ -3,5 +3,6 @@ package edu.sjsu.assignment0;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CS49J!");
+        // an unneccessary change
     }
 }
